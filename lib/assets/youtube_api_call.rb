1c99b095b174309d1401eab5e74f35f139018149
brsoff <brsoff@gmail.com>
2014-01-31 16:24:46 -0500

    adding youtube api call script

---
+++ lib/assets/youtube_api_call.rb
@@ -0,0 +1,14 @@
+require 'json'
+require 'pry'
+require 'httparty'
+require 'active_support/core_ext/hash'
+
+results = HTTParty.get('http://gdata.youtube.com/feeds/api/standardfeeds/US/most_popular?v=2&time=today&max-results=5')
+
+results_json = Hash.from_xml(results).to_json
+
+f = File.open('json.txt', 'w') do |f| 
+  f.write(results_json) 
+end
+
+binding.pry
