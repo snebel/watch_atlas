e1ba539056a0127744126909434ac8bcdd05be3b
snebel <sam@samnebel.com>
2014-02-02 15:12:28 -0500

    refactored index page and you_tube embed method

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,17 @@
 class Video < ActiveRecord::Base
   has_many :country_videos
   has_many :countries, through: :country_videos
+
+  def youtube_embed
+    normal_url = self.normal_url
+    if normal_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else 
+      normal_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/] 
+      youtube_id = $5 
+    end 
+    "http://www.youtube.com/embed/#{youtube_id}"
+  end
+
+
 end
