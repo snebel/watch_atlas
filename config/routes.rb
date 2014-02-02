1df1f3b6fe2b41ec06ae028386613ee5124eb1fc
nhindman <nathaniel.hindman@gmail.com>
2014-02-01 22:42:57 -0500

    top 5 vids for each country showing up index pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 WdiProject2::Application.routes.draw do
+
+  root to: "countries#index"
+
+  resources :countries
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
