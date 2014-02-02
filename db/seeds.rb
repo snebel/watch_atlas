8b2edbf13c1892225d0e14db54bb53361655b148
snebel <sam@samnebel.com>
2014-02-02 17:23:04 -0500

    edited seed task to create countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# don't want to create countries each time
 
-#require 'active_support/core_ext/hash'
+Country.delete_all
 Video.delete_all
 CountryVideo.delete_all
+
+countries = {"DZ" => "ALGERIA", "AT" => "AUSTRIA", "AU" => "AUSTRALIA", "AR" => "ARGENTINA", "BH" => "BAHRAIN", "BE" => "BELGIUM", "BR" => "BRAZIL", "CA" => "CANADA", "CL" => "CHILE", "CO" => "COLOMBIA", "CZ" => "CZECH REPUBLIC", "DK" => "DENMARK", "EG" => "EGYPT", "FI" => "FINLAND", "FR" => "FRANCE", "DE" => "GERMANY", "GH" => "GHANA", "GR" => "GREECE", "HK" => "HONG KONG", "HU" => "HUNGARY", "IN" => "INDIA", "ID" => "INDONESIA", "IE" => "IRELAND", "IL" => "ISRAEL", "IT" => "ITALY", "JP" => "JAPAN", "JO" => "JORDAN", "KE" => "KENYA", "KR" => "SOUTH KOREA", "KW" => "KUWAIT", "MY" => "MALAYSIA", "MX" => "MEXICO", "MA" => "MOROCCO", "NL" => "NETHERLANDS", "NO" => "NORWAY", "OM" => "OMAN", "PE" => "PERU", "PH" => "PHILIPPINES", "PL" => "POLAND", "PT" => "PORTUGAL", "QA" => "QATAR", "RO" => "ROMANIA", "RU" => "RUSSIAN", "SA" => "SAUDI ARABIA", "SN" => "SENEGAL", "SG" => "SINGAPORE", "SK" => "SLOVAKIA", "ZA" => "SOUTH AFRICA", "ES" => "SPAIN", "SE" => "SWEDEN", "CH" => "SWITZERLAND", "TW" => "TAIWAN", "TN" => "TUNISIA", "TR" => "TURKEY", "UG" => "UGANDA", "UA" => "UKRAINE", "AE" => "UNITED ARAB EMIRATES", "GB" => "UNITED KINGDOM", "US" => "UNITED STATES", "YE" => "YEMEN"}
+
+countries.each do |code, name|
+  Country.create(code: code, name: name)
+end
+
+
+
+
+
+
+
+
 
 # What do we want?
 #   -title: results_json["feed"]["entry"]["title"]
 #   -big-vid url: results_json["feed"]["entry"]["content"]["src"]
 #   -normal-vid url: results_json["feed"]["entry"]["link"].first["href"]
 #   -term/label: results_json["feed"]["entry"]["category"][1]["term"]
+
+
+
+
 
 Country.all.each do |country|
 # grab top videos for country here
