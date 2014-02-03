25bfd3d693b22a6a687bd855a86159518fd90609
snebel <sam@samnebel.com>
2014-02-03 18:56:47 -0500

    formatted js

--- public/map-home.js
+++ public/map-home.js
@@ -36,51 +36,46 @@ g.append("path")
 
 
 function countryClick(d) {
-    var self = this;
+  var self = this;
 
-    $.ajax({
-      method: 'get',
-      url: '/countries/maps/' + d.id,
-      dataType: 'json'
+  $.ajax({
+    method: 'get',
+    url: '/countries/maps/' + d.id,
+    dataType: 'json'
+  })
+    .success(function (data) {
+      zoomIn();
+      makeTooltip(data, true);
     })
-      .success(function (data) {
-        zoomIn();
-        makeTooltip(data, true);
-      })
-      .fail(function(data){
-        makeTooltip(data, false);  
+    .fail(function(data){
+      makeTooltip(data, false);  
+    });
+
+  function zoomIn() {
+    if (active === d){ return reset();}
+    g.selectAll('.active').classed('active', false);
+    d3.select(self).classed('active', active = d);
+
+    var b = path.bounds(d);
+    g.transition().duration(750).attr('transform',
+        'translate(' + projection.translate() + ')' + 'scale(' + .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ')' + 'translate(' + -(b[1][0] + b[0][0]) / 2 + ',' + -(b[1][1] + b[0][1]) / 2 + ')');
+    //end zoom stuff
+  }
+
+  var page_y = event.pageY;
+  var page_x = event.pageX;
+
+  function makeTooltip(data, good) { //data => [country, vid1, vid2,...]  
+    return tooltip
+      .style('visibility', 'visible')
+      .style('display', 'block')
+      .style('top', (page_y + 'px'))
+      .style('left', (page_x - 10 + 'px'))
+      .html(function () {
+        if (good) { return htmlSuccessGen(data); }
+        else { return htmlFailGen(); }
       });
-
-      function zoomIn() {
-        if (active === d){ return reset();}
-        g.selectAll('.active').classed('active', false);
-        d3.select(self).classed('active', active = d);
-
-        var b = path.bounds(d);
-        g.transition().duration(750).attr('transform',
-            'translate(' + projection.translate() + ')' + 'scale(' + .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ')' + 'translate(' + -(b[1][0] + b[0][0]) / 2 + ',' + -(b[1][1] + b[0][1]) / 2 + ')');
-        //end zoom stuff
-      }
-
-        var page_y = event.pageY;
-        var page_x = event.pageX;
-
-        function makeTooltip(data, good) { //data => [country, vid1, vid2,...]  
-            return tooltip
-              .style('visibility', 'visible')
-              .style('display', 'block')
-              .style('top', (page_y + 'px'))
-              .style('left', (page_x - 10 + 'px'))
-              .html(function () {
-                if (good) {
-                  return htmlGen(data);
-                }
-                else {
-                  return htmlGenNoData();
-                }
-              });
-        }
-    //}
+  }
 }
 
 
@@ -103,133 +98,116 @@ queue()
 
 function ready(error, world) {
 
-    var countries = topojson.feature(world, world.objects.countries).features
+  var countries = topojson.feature(world, world.objects.countries).features
 
-    g.selectAll('.country')
-        .data(countries)
-        .enter().insert('path')
-        .attr('class', 'country')
-        .attr('d', path)
-        .attr('id', function (d) {
-            return 'id_' + d.id
-        })
-        .style('fill', '#95a5a6')
-        .on('mouseover', function (d) {
-            d3.select('path#id_' + d.id)
-                .style('fill', '#d35400')
-        })
-        .on('mouseout', function (d) {
-
-            if (valid_map_ids.indexOf(d.id) != -1) {
-                d3.select('path#id_' + d.id)
-                    .style('fill', '#16a085')
-            } else {
-                d3.select('path#id_' + d.id)
-                    .style('fill', '#95a5a6')
-            }
-        })
-        .on('click', countryClick)
-
+  g.selectAll('.country')
+    .data(countries)
+    .enter().insert('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .attr('id', function(d){
+        return 'id_' + d.id
+    })
+    .style('fill', '#95a5a6')
+    .on('mouseover', function(d){
+      d3.select('path#id_' + d.id)
+        .style('fill', '#d35400')
+    })
+    .on('mouseout', function(d){
+      if (valid_map_ids.indexOf(d.id) != -1) {
+        d3.select('path#id_' + d.id).style('fill', '#16a085')
+      } 
+      else {
+        d3.select('path#id_' + d.id).style('fill', '#95a5a6')
+      }
+    })
+    .on('click', countryClick);
 
     g.selectAll('path.globewater')
-        .call(d3.behavior.drag()
+      .call(d3.behavior.drag()
 
-            .origin(function () {
-                var r = projection.rotate();
-                return {
-                    x: r[0] / sens,
-                    y: -r[1] / sens
-                };
-            })
-            .on('drag', function () {
-                console.log('dragging')
-                var rotation_amount = 50;
-                console.log(d3.event);
-                var rotate = projection.rotate();
-                projection.rotate([(rotate[0] + rotation_amount), 0, 0]);
-                g.selectAll('path.country')
-                    .attr('d', path);
-                g.selectAll('.focused').classed('focused', focused = false);
-            }))
-
-
-    htmlGen = function (data) {
-
-        var contents = '<h3>' + data[0].name + '</h3>';
-        contents += '<ul class="box-videos">'
-
-        for (var i = 1; i < data.length; i++) {
-            contents += '<li><img src="' + data[i].thumbnail_url + '" style="float:left; margin:0 8px 5px 0;" /><a target="_blank" href="' + data[i].normal_url + '">"' + data[i].title + '</a></li>'
+      .origin(function () {
+        var r = projection.rotate();
+        return {
+          x: r[0] / sens,
+          y: -r[1] / sens
         };
+      })
+      .on('drag', function () {
+        console.log('dragging')
+        var rotation_amount = 50;
+        console.log(d3.event);
+        var rotate = projection.rotate();
+        projection.rotate([(rotate[0] + rotation_amount), 0, 0]);
+        g.selectAll('path.country')
+            .attr('d', path);
+        g.selectAll('.focused').classed('focused', focused = false);
+      }));
 
-        contents += '</ul>'
-        contents += '<div class="close-me">close</div>'
 
-        $('body').on('click', '.close-me', function () {
-            $('.tooltip').hide();
-            reset();
-        });
+  htmlSuccessGen = function (data) {
+    var contents = '<h3> <a href=/countries/' + data[0].id + '>' + data[0].name + '</a></h3>';
+    contents += '<ul class="box-videos">'
 
-        return contents;
-    }
+    for (var i = 1; i < data.length; i++) {
+      contents += '<li><img src="' + data[i].thumbnail_url + '" style="float:left; margin:0 8px 5px 0;" /><a target="_blank" href="' + data[i].normal_url + '">"' + data[i].title + '</a></li>'
+    };
+
+    contents += '</ul>'
+    contents += '<div class="close-me">close</div>'
+
+    $('body').on('click', '.close-me', function () {
+      $('.tooltip').hide();
+      reset();
+    });
+
+    return contents;
+  }
 
 
-    htmlGenNoData = function () {
-        var contents = 'Sorry, there is no YouTube data <br>for this country';
-        contents += '<div class="close-me">close</div>'
+  htmlFailGen = function () {
+    var contents = 'Sorry, there is no YouTube data <br>for this country';
+    contents += '<div class="close-me">close</div>'
 
-        $('body').on('click', '.close-me', function () {
-            $('.tooltip').hide();
-            reset();
-        });
+    $('body').on('click', '.close-me', function () {
+      $('.tooltip').hide();
+      reset();
+    });
 
-        return contents;
-    }
-
+    return contents;
+  }
 
     //populate active countries with different color
 
     valid_map_ids.forEach(function (x) {
-        d3.select('path#id_' + x)
-            .style('fill', '#16a085')
-            .style('cursor', 'pointer')
+      d3.select('path#id_' + x)
+        .style('fill', '#16a085')
+        .style('cursor', 'pointer')
     });
 
-
-
-
-    console.log(valid_map_ids)
-
     d3.select('#right-rotator')
-        .on('click', function () {
-            var rotate = projection.rotate();
-            var rotation_amount = 40
-            var x_point = d3.event.x
-            var y_point = d3.event.y
-            console.log(rotate);
-            projection.rotate([(rotate[0] - rotation_amount), 0, 0]);
-            g.selectAll('path.country')
-                .attr('d', path);
-            // g.selectAll('.focused').classed('focused', focused = false);
-        });
+      .on('click', function () {
+        var rotate = projection.rotate();
+        var rotation_amount = 40;
+        var x_point = d3.event.x;
+        var y_point = d3.event.y;
+        console.log(rotate);
+        projection.rotate([(rotate[0] - rotation_amount), 0, 0]);
+        g.selectAll('path.country').attr('d', path);
+      });
 
 
     d3.select('#left-rotator')
-        .on('click', function () {
-            var rotate = projection.rotate();
-            var rotation_amount = 40
-            var x_point = d3.event.x
-            var y_point = d3.event.y
-            console.log(rotate);
-            projection.rotate([(rotate[0] + rotation_amount), 0, 0]);
-            g.selectAll('path.country')
-                .attr('d', path);
-            // g.selectAll('.focused').classed('focused', focused = false);
-        });
+      .on('click', function () {
+        var rotate = projection.rotate();
+        var rotation_amount = 40;
+        var x_point = d3.event.x;
+        var y_point = d3.event.y;
+        console.log(rotate);
+        projection.rotate([(rotate[0] + rotation_amount), 0, 0]);
+        g.selectAll('path.country').attr('d', path);
+      });
 
 }
-
-
-
 
 d3.select(self.frameElement).style('height', height + 'px');
