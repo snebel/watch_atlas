58887cc3022a00f06171bdef9e933152df97aac0
brsoff <brsoff@gmail.com>
2014-02-03 14:20:28 -0500

    i dunno

--- public/map-home.js
+++ public/map-home.js
@@ -2,6 +2,8 @@
 
 var country_json_data = $.parseJSON(gon.country_json)
 
+// all of the countries in the database
+
 var valid_map_ids = [12, 887, 40, 36, 32, 48, 56, 76, 124, 152, 170, 203, 208, 818, 246, 250, 276, 288, 300, 344, 348, 356, 360, 372, 376, 380, 392, 400, 404, 410, 414, 458, 484, 504, 528, 578, 512, 604, 608, 616, 620, 634, 642, 643, 682, 686, 702, 703, 710, 724, 752, 756, 158, 788, 792, 800, 804, 784, 826, 840];
 
 var width = 960,
@@ -10,7 +12,7 @@ var width = 960,
   focused,
   active;
 
-var projection = d3.geo.orthographic()
+var projection = d3.geo.mercator()
   .translate([width / 2, height / 2])
   .scale(300)
   .precision(.1)
