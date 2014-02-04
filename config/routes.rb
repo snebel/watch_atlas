23e8dd08c4dd6e98f49c07dd95eba2ec7e26826b
Patrick Morgan <pamorgan12@gmail.com>
2014-02-04 15:39:26 -0500

    figuring out video show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ WdiProject2::Application.routes.draw do
   root to: "countries#index"
 
   resources :countries
+  resources :videos, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,5 +11,4 @@ WdiProject2::Application.routes.draw do
   get '/countries/maps/:map_id' => 'countries#get_country_data'
 
   get '/parallax' => 'parallax#index'
-
 end
