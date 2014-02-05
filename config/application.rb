054da652fb95015760ffd60ef71e09a17f0c404f
nhindman <nathaniel.hindman@gmail.com>
2014-02-05 14:06:55 -0500

    changed precompile to true

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module WdiProject2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
